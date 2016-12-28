8e4acfb5a670ed4e71aeac1a879f24515cd479ca
Jordan Howlett <jordan@howlett.io>
2016-12-28 12:55:01 -0500

    tried brunch

--- brunch-config.js
+++ brunch-config.js
@@ -15,16 +15,18 @@ exports.config = {
   files: {
     javascripts: {
       joinTo: {
-        'js/index.js': /^app\/js\/index\//,
-        'js/pizza.js': /^app\/js\/pizza\//
+        'js/app.js': 'app/js/*.js'
+        // 'js/index.js': /^app\/js\/index\//,
+        // 'js/pizza.js': /^app\/js\/pizza\//
       }
     },
     stylesheets: {
       joinTo: {
-        'css/style.css': /^app\/css\/style\//,
-        'css/print.css': /^app\/css\/print\//,
-        'css/bootstrap.css': /^app\/css\/bootstrap\//,
-        'css/pizza.css': /^app\/css\/pizza\//
+        // 'css/app.css': 'app/css/pizza/*.css'
+        'css/print.css': 'app/css/site/print.css',
+        'css/style.css': 'app/css/site/style.css',
+        'css/bootstrap.css': 'app/css/bootstrap/*.css',
+        'css/pizza.css': 'app/css/pizza/*.css'
       }
     }
   }
